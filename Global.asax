33fbbf893fc2867621c13f1e75bebc8557db6b23
ManvirSeth <31705053+ManvirSeth@users.noreply.github.com>
2017-09-20 16:11:40 -0400

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Futurevalue.Global" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Futurevalue.Startup))]
+namespace Futurevalue
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
